7e337b3d7e5b990913dd242ff8aa5ec6c10194e6
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2023-04-07 22:43:04 +0200

    object

--- graphe.py
+++ graphe.py
@@ -1,31 +1,37 @@
-def creer_matrice(contraintes):
+class Graphe:
     
-    # Déterminer le nombre de tâches N
-    N = len(contraintes)
-
-    # Créer une matrice carrée de taille N+2 pour représenter le graphe,
-    # en ajoutant les deux sommets fictifs a et w
-    matrice = [[0 for j in range(N+2)] for i in range(N+2)]
-
-    # Remplir la matrice avec les valeurs correspondantes
-    for ligne in contraintes:
-        tache_info = ligne.split()
-        tache = int(tache_info[0])
-        duree = int(tache_info[1])
-        contraintes = []
-        if len(tache_info) > 2:
-            contraintes = list(map(int, tache_info[2:]))
-        # Ajouter la tâche et ses contraintes à la matrice
-        matrice[0][tache] = 0
-        matrice[tache][tache] = duree
-        for c in contraintes:
-            matrice[c][tache] = 0
-        matrice[tache][N+1] = 0
-
-    return matrice
-
-def afficher_matrice(matrice):
-    for i in range(N+2):
-        for j in range(N+2):
-            print(matrice[i][j], end=' ')
-        print()
+        def __init__(self, contraintes):
+            self.matrice = self.creer_matrice(contraintes)
+            self.N = len(contraintes)
+    
+        def creer_matrice(self, contraintes):
+            # Créer une matrice carrée de taille N+2 pour représenter le graphe,
+            # en ajoutant les deux sommets fictifs a et w
+            self.N = len(contraintes)
+            print(self.N)
+            matrice = [[0 for j in range(self.N+2)] for i in range(self.N+2)]
+    
+            # Remplir la matrice avec les valeurs correspondantes
+            for ligne in contraintes:
+                tache_info = ligne.split()
+                tache = int(tache_info[0])
+                duree = int(tache_info[1])
+                contraintes = []
+                if len(tache_info) > 2:
+                    contraintes = list(map(int, tache_info[2:]))
+                # Ajouter la tâche et ses contraintes à la matrice
+                matrice[0][tache] = 0
+                matrice[tache][tache] = duree
+                for c in contraintes:
+                    matrice[c][tache] = 0
+                matrice[tache][self.N+1] = 0
+    
+            return matrice
+    
+        def afficher_matrice(self):
+            for i in range(self.N +2):
+                print(i, end=' ')
+            for i in range(self.N +2):
+                for j in range(self.N +2):
+                    print(self.matrice[i][j], end=' ')
+                print()
--- main.py
+++ main.py
@@ -6,39 +6,39 @@ while True: # boucle infinie
     if decision.lower() == "non":  # si l'utilisateur ne veut plus tester
         break  # sortir de la boucle
 
-# sinon, demander à l'utilisateur de choisir un tableau de contraintes
-nom_fichier = input("Choisissez le nom du fichier contenant les contraintes à traiter : ")
+    # sinon, demander à l'utilisateur de choisir un tableau de contraintes
+    nom_fichier = input("Choisissez le nom du fichier contenant les contraintes à traiter : ")
 
-# lire le fichier et stocker les contraintes en mémoire
-contraintes = []
-with open(nom_fichier, 'r') as f:
-    for ligne in f:
-        contraintes.append(ligne.strip())
+    # lire le fichier et stocker les contraintes en mémoire
+    contraintes = []
+    with open(nom_fichier, 'r') as f:
+        for ligne in f:
+            contraintes.append(ligne.strip())
 
-# créer la matrice correspondant au graphe et l'afficher
-matrice = creer_matrice(contraintes)  # fonction à créer
-afficher_matrice(matrice)  # fonction à créer
+    # créer la matrice correspondant au graphe et l'afficher
+    graphe = Graphe(contraintes)
+    graphe.afficher_matrice()
 
-# vérifier si les propriétés pour un graphe d'ordonnancement sont vérifiées
-if est_graphe_ordonnancement(matrice):  #TODO: fonction à créer
-    # calculer les rangs des sommets et les afficher
-    rangs = calculer_rangs(matrice)  #TODO: fonction à créer
-    afficher_rangs(rangs)  #TODO: fonction à créer
-    
-    # calculer les calendriers au plus tôt et au plus tard et les afficher
-    au_plus_tot, au_plus_tard = calculer_calendriers(matrice)  #TODO: fonction à créer
-    afficher_calendriers(au_plus_tot, au_plus_tard)  #TODO: fonction à créer
-    
-    # calculer les marges et les afficher
-    marges = calculer_marges(au_plus_tot, au_plus_tard)  #TODO: fonction à créer
-    afficher_marges(marges)  #TODO: fonction à créer
-    
-    # calculer le(s) chemin(s) critique(s) et les afficher
-    chemins_critiques = trouver_chemins_critiques(marges)  #TODO: fonction à créer
-    afficher_chemins_critiques(chemins_critiques)  #TODO: fonction à créer
-    
-else:  # si les propriétés ne sont pas vérifiées
-    # proposer à l'utilisateur de changer de tableau de contraintes
-    print("Le graphe ne vérifie pas les propriétés pour un graphe d'ordonnancement. Veuillez choisir un autre tableau de contraintes.")
+    # # vérifier si les propriétés pour un graphe d'ordonnancement sont vérifiées
+    # if est_graphe_ordonnancement(matrice):  #TODO: fonction à créer
+    #     # calculer les rangs des sommets et les afficher
+    #     rangs = calculer_rangs(matrice)  #TODO: fonction à créer
+    #     afficher_rangs(rangs)  #TODO: fonction à créer
+        
+    #     # calculer les calendriers au plus tôt et au plus tard et les afficher
+    #     au_plus_tot, au_plus_tard = calculer_calendriers(matrice)  #TODO: fonction à créer
+    #     afficher_calendriers(au_plus_tot, au_plus_tard)  #TODO: fonction à créer
+        
+    #     # calculer les marges et les afficher
+    #     marges = calculer_marges(au_plus_tot, au_plus_tard)  #TODO: fonction à créer
+    #     afficher_marges(marges)  #TODO: fonction à créer
+        
+    #     # calculer le(s) chemin(s) critique(s) et les afficher
+    #     chemins_critiques = trouver_chemins_critiques(marges)  #TODO: fonction à créer
+    #     afficher_chemins_critiques(chemins_critiques)  #TODO: fonction à créer
+        
+    # else:  # si les propriétés ne sont pas vérifiées
+    #     # proposer à l'utilisateur de changer de tableau de contraintes
+    #     print("Le graphe ne vérifie pas les propriétés pour un graphe d'ordonnancement. Veuillez choisir un autre tableau de contraintes.")
 
 
