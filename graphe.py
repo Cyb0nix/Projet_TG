bcab34bdfaab9fb38222b6ab8526cfcdd7d309be
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2023-04-07 23:58:24 +0200

    creer matrice et afficher matrice

--- graphe.py
+++ graphe.py
@@ -1,37 +1,56 @@
 class Graphe:
     
         def __init__(self, contraintes):
-            self.matrice = self.creer_matrice(contraintes)
             self.N = len(contraintes)
+            self.duree = {}
+            self.matrice = self.creer_matrice(contraintes)
     
         def creer_matrice(self, contraintes):
             # Créer une matrice carrée de taille N+2 pour représenter le graphe,
             # en ajoutant les deux sommets fictifs a et w
             self.N = len(contraintes)
             print(self.N)
-            matrice = [[0 for j in range(self.N+2)] for i in range(self.N+2)]
+            matrice = [["*" for j in range(self.N+2)] for i in range(self.N+2)]
     
             # Remplir la matrice avec les valeurs correspondantes
             for ligne in contraintes:
                 tache_info = ligne.split()
                 tache = int(tache_info[0])
                 duree = int(tache_info[1])
+                self.duree[tache] = duree
+            
+            for ligne in contraintes:
                 contraintes = []
+                tache_info = ligne.split()
+                tache = int(tache_info[0])
                 if len(tache_info) > 2:
                     contraintes = list(map(int, tache_info[2:]))
-                # Ajouter la tâche et ses contraintes à la matrice
-                matrice[0][tache] = 0
-                matrice[tache][tache] = duree
-                for c in contraintes:
-                    matrice[c][tache] = 0
-                matrice[tache][self.N+1] = 0
+                    print(contraintes)
+                if contraintes == []:
+                    matrice[0][tache] = 0
+                else:
+                    for c in contraintes:
+                        matrice[c][tache] = self.duree[c]
     
             return matrice
     
         def afficher_matrice(self):
-            for i in range(self.N +2):
-                print(i, end=' ')
-            for i in range(self.N +2):
-                for j in range(self.N +2):
-                    print(self.matrice[i][j], end=' ')
+            print("Matrice des valeurs")
+            print("    ", end="")
+            for i in range(self.N+2):
+                if i < 10:
+                    print(" " + str(i) + "  ", end="")
+                else:
+                    print(str(i) + "  ", end="")
+            print("\n", end='')
+            for i in range(self.N+2):
+                if i < 10:
+                    print(" " + str(i) + " ", end="")
+                else:
+                    print(str(i) + " ", end="")
+                for j in range(self.N+2):
+                    if j < 10:
+                        print("  " + str(self.matrice[i][j]) + " ", end="")
+                    else:
+                        print("  " + str(self.matrice[i][j]) + " ", end="")
                 print()
